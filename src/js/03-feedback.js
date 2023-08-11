95c408e761f7f867db6300cc405c8d77b2a34b67
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-08-11 12:09:30 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,8 +2,8 @@ import throttle from 'lodash.throttle';
 
 // Get the form and form fields
 const feedbackForm = document.querySelector('.feedback-form');
-const emailField = feedbackForm.querySelector('[name="email"]');
-const messageField = feedbackForm.querySelector('[name="message"]');
+const emailInput = feedbackForm.querySelector('[name="email"]');
+const messageInput = feedbackForm.querySelector('[name="message"]');
 
 // Define the local storage key
 const storageKey = 'feedback-form-state';
@@ -11,8 +11,8 @@ const storageKey = 'feedback-form-state';
 // Function to save form field values to local storage
 function saveFormState() {
     const state = {
-        email: emailField.value,
-        message: messageField.value,
+        email: emailInput.value,
+        message: messageInput.value,
     };
     localStorage.setItem(storageKey, JSON.stringify(state));
 }
@@ -22,16 +22,16 @@ function loadFormState() {
     const savedState = localStorage.getItem(storageKey);
     if (savedState) {
         const state = JSON.parse(savedState);
-        emailField.value = state.email || '';
-        messageField.value = state.message || '';
+        emailInput.value = state.email || '';
+        messageInput.value = state.message || '';
     }
 }
 
 // Function to clear local storage and form fields
-function clearForm() {
+function clearFormState() {
     localStorage.removeItem(storageKey);
-    emailField.value = '';
-    messageField.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
 }
 
 // Add event listener for input events on the form fields (throttled for once every 500 milliseconds)
@@ -39,11 +39,17 @@ feedbackForm.addEventListener('input', throttle(() => {
     saveFormState();
 }, 500));
 
-// Add event listener for form submission
+// Event listener for form submission
 feedbackForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const state = JSON.parse(localStorage.getItem(storageKey));
-    console.log('Form submission data:', state);
-    clearForm();
+    const submittedData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    clearFormState();
+    console.log('Submitted data:');
+    console.log(submittedData);
 });
 
+// Load the stored data on page load
+loadFormState();
